9ba8c36a812891b6c2c6effd39d2faeaf564ed4c
sffool <149025264+sffool@users.noreply.github.com>
2024-08-07 01:22:43 +0000

    github action update Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,1 @@
-var posts=["2024/08/06/CTF show/","2024/08/06/DASCTF 2024/","2024/08/06/HCTF 2nd/","2024/08/03/test/"];function toRandomPost(){
-    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
-  };
+var posts=["2024/08/06/DASCTF 2024/","2024/08/03/test/"];function toRandomPost(){pjax.loadUrl("/"+posts[Math.floor(Math.random()*posts.length)])}
