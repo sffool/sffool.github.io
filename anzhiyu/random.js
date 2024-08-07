cebe18b4e01107629714f55d6f720d8daba11ffe
sffool <sffool@users.noreply.github.com>
2024-08-07 01:15:31 +0000

    Updated By Yueque

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/06/Web/题目/CTF show/","2024/08/06/Web/题目/HCTF 2nd/","2024/08/06/Reverse/题目/DASCTF 2024/"];function toRandomPost(){
+var posts=["2024/08/06/CTF show/","2024/08/06/HCTF 2nd/","2024/08/06/DASCTF 2024/","2024/08/03/test/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
